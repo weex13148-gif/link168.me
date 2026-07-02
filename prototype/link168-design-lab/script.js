303c6d5eb4eaa4341e13302c9761058b789ebdb0
weex13148-gif <weex13148@gmail.com>
2026-07-02 18:14:11 +0800

    chore: remove obsolete docs prototypes and deploy scripts

--- prototype/link168-design-lab/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach((link) => {
-  link.addEventListener("click", (event) => {
-    const target = document.querySelector(link.getAttribute("href"));
-    if (!target) return;
-    event.preventDefault();
-    target.scrollIntoView({ behavior: "smooth", block: "start" });
-  });
-});
